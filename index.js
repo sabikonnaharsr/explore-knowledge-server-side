4464b438ca8d93f92ec97c0ee9f96fa4867bc09e
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-27 15:06:19 +0600

    added category

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const Port = process.env.Port || 5000;
 app.use(cors());
 
-const Port = process.env.Port || 5000;
-const bookCategory = require("./data/booksCategory.json");
 const books = require("./data/books.json");
+
+const category = require("./data/booksCategory.json");
+
 
 app.get("/", (req, res) => {
   res.send("Now server is running...");
 });
 
+
 app.get("/categories", (req, res) => {
-  res.send(bookCategory);
+  res.send(category);
 });
+
+
 
 app.get("/books", (req, res) => {
   res.send(books);
 });
 
+
+
 app.get("/books/:id", (req, res) => {
   const id = req.params.id;
   const getSingleItem = books?.filter((p) => p.uid == id);
   res.send(getSingleItem);
-
-  //   if (!getSingleItem) {
-  //     res.send(getSingleItem);
-  //   }
 });
 
-app.get("/booksCategory/:name", (req, res) => {
-  const getCategory = bookCategory?.filter((p) => p.category == name);
-  res.send(getCategory);
-});
 
 app.listen(Port, () => {
   console.log("server is running", Port);
